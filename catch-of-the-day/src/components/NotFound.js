1a5d3224d5ec606361b9aff55371355c5dd5f23c
an n <anthedev@gmail.com>
2018-09-03 20:35:03 -0700

    9. Routing with React Router

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Stateless functional component
+const NotFound = () => (
+  <div>
+    <h2>Not Found?!?@??!?</h2>
+  </div>
+)
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -7,11 +7,16 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // import components
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+// import StorePicker from './components/StorePicker';
+// import App from './components/App';
+
+// Don't need to import other components when
+// importing and rendering Router component:
+import Router from "./components/Router";
 
 // import css
 import "./css/style.css";
 
+
 // use render() method to render html to page
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
