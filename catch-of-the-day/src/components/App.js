3706b9e1695d84c7ce8bb804e0c33e3dc7f871c0
An Nguyen <an@still-water.com>
2018-09-13 00:43:41 -0700

    15. Displaying State with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "./Header.js";
-import Order from "./Order.js";
-import Inventory from "./Inventory.js";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes"
-
+import Fish from "./Fish";
 class App extends React.Component {
 
   state = {
@@ -39,6 +39,11 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
