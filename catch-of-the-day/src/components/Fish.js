150845ace1e4f7a44fcc8fd34033a92678f5d926
An Nguyen <an@still-water.com>
2018-09-26 18:00:34 -0700

    15. Displaying State with JSX - Review

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { formatPrice } from "../helpers";
 
+// How does data get anywhere? Props!
+
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    // now:
+     // { image } = { this.props.details.image }
+     // { name } = { this.props.details.name }
+     // etc.
+
     return (
       <li className="menu-fish">
         <img src={ image } alt={ name } />
@@ -14,7 +21,7 @@ class Fish extends React.Component {
         </h3>
 
         <p>{ desc }</p>
-        
+
         <button>Add to Cart</button>
       </li>
     )
