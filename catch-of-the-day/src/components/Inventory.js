84e10d573edbedbbbf33477d7e1f7847500a617d
An Nguyen <an@still-water.com>
2019-09-11 18:22:10 -0700

    24 - Authentication - authHandler

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 import Login from "./Login";
+import { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -13,8 +15,17 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   }
 
+  authHandler = async authData => {
+    console.log(authData);
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+  };
+
   render() {
-    return <Login />;
+    return <Login authenticate={this.authenticate} />;
 
     return (
       <div className="inventory">
